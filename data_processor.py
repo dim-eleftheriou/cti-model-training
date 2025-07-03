ca573a25abef07d1a8b73e760c9246db8b1d8623
Dimitrios Eleftheriou <dimelf93@gmail.com>
2025-07-03 09:55:42 +0000

    Creating grpo depedencies and functions

--- data_processor.py
+++ data_processor.py
@@ -2,6 +2,7 @@ import os
 import json
 import datasets
 
+
 class SplittedJsonIoDataset:
 
     def __init__(self, tokenizer, config):
@@ -10,7 +11,6 @@ class SplittedJsonIoDataset:
         self.io_dataset_path = config["io_dataset_path"]
         self.train_path = os.path.join(config["io_dataset_path"], "train")
         self.eval_path = os.path.join(config["io_dataset_path"], "validation")
-
 
     @staticmethod
     def load_single_example(path:str, filename:str):
@@ -45,7 +45,40 @@ class SplittedJsonIoDataset:
         # Create a hf dataset dict
         dataset = datasets.DatasetDict({"train":hf_train, "eval":hf_eval})
         return dataset
+
+    @staticmethod
+    def serialize(data:list):
+        data_serialized = [
+            {"input": example["input"],
+            "output": json.dumps(example["output"])} for example in data]
+        return data_serialized
     
+    def grpo_format_dataset(self, dataset:datasets.Dataset):
+        formatted_dataset = dataset.map(lambda x: {
+            'prompt': [
+                {'role': 'system', 'content': self.system_message},
+                {'role': 'user', 'content': x['input']}
+            ],
+            'answers': x['output']
+        })
+        return formatted_dataset.remove_columns(['input', 'output'])
+
+    def grpo_create(self):
+        # Load raw data
+        raw_train_list, raw_eval_list = self.load_raw_data()
+        # List of data must be serialized. "output" should be str
+        train_data_serialized = self.serialize(raw_train_list)
+        eval_data_serialized = self.serialize(raw_eval_list)
+        # Create HF datasets
+        train_dataset = datasets.Dataset.from_list(train_data_serialized)
+        eval_dataset = datasets.Dataset.from_list(eval_data_serialized)
+        # Format for GRPO training
+        train_dataset = self.grpo_format_dataset(train_dataset)
+        eval_dataset = self.grpo_format_dataset(eval_dataset)
+        # Create a hf dataset dict
+        dataset = datasets.DatasetDict({"train":train_dataset, "eval":eval_dataset})
+        return dataset
+
 def filter_by_token_counts(dataset, tokenizer, config):
     if config["max_seq_length"]:
         limit = config["max_seq_length"]
